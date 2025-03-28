6c387bdce062c01ddc7bbafca76bd5137377d7f2
five-sun <tyo1012@naver.com>
2025-03-28 17:40:58 +0900

    rabbitmq 설정

--- API-Gateway/src/main/java/com/kitcha/gateway/filter/JwtFilter.java
+++ API-Gateway/src/main/java/com/kitcha/gateway/filter/JwtFilter.java
@@ -30,7 +30,7 @@ public class JwtFilter implements GlobalFilter, Ordered {
         String path = exchange.getRequest().getURI().getPath();
 
         // 인증 서비스는 토큰이 없어도 통과
-        if (path.startsWith("/authentication/users") && !path.startsWith("/authentication/users/interest")) {
+        if ((path.startsWith("/authentication/users") && !path.startsWith("/authentication/users/interest")) || path.contains("/actuator/**")) {
             return chain.filter(exchange);
         }
 
