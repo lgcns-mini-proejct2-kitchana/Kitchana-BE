64f5df9348ae8a97589547dbbc078144430f5bcb
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-03-31 17:48:58 +0900

    test: 젠킨슨 테스트

--- Config-server/src/main/java/com/kitcha/configserver/ConfigServerApplication.java
+++ Config-server/src/main/java/com/kitcha/configserver/ConfigServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// 젠킨슨 테스트 1
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
--- eureka/src/main/java/com/kitcha/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/kitcha/eureka/EurekaApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// 젠킨슨 테스트 1
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
