98bf681848b1afe33543cf59788a253b11067d2f
Tae Jeong Kim <oganesson12@hufs.ac.kr>
2025-04-01 18:06:07 +0900

    test: ApiGateway 젠킨스 빌드 테스트

--- API-Gateway/src/main/java/com/kitcha/gateway/ApiGatewayApplication.java
+++ API-Gateway/src/main/java/com/kitcha/gateway/ApiGatewayApplication.java
@@ -3,7 +3,7 @@ package com.kitcha.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 젠킨슨 테스트 1
+// 젠킨슨 테스트 29번째 빌드
 
 @SpringBootApplication
 public class ApiGatewayApplication {
